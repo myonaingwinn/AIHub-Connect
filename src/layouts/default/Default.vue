408c9501d5b9081ffdada32de28594826bd8c6df
myonaingwinn <myonaingwin@ymail.ne.jp>
2023-06-21 09:39:10 +0900

    init

---
+++ src/layouts/default/Default.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <default-bar />
+    <drawer />
+    <default-view />
+  </v-app>
+</template>
+
+<script setup>
+import DefaultBar from "./AppBar.vue";
+import DefaultView from "./View.vue";
+import Drawer from "../Drawer.vue";
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- <HelloWorld /> -->
+
+  <!-- <v-card title="Card title" subtitle="Subtitle" text="...">
+    <v-card-actions>
+      <v-btn>Click me</v-btn>
+    </v-card-actions>
+  </v-card> -->
+</template>
+
+<script setup>
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
