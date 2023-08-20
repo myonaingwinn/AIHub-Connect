5110dde123c09401c604b65a3a349cf9e29e0c4e
myonaingwinn <myonaingwinn081@gmail.com>
2023-08-20 22:16:14 +0900

    update : app description

--- src/components/NameDescription.vue
+++ src/components/NameDescription.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-name text-h2 font-weight-black">AIHub Connect</div>
   <div v-if="!description" class="app-description mt-9">
-    Your gateway to AI-powered connections
+    Your gateway to AI-powered experiences
   </div>
 </template>
 
